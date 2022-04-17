3b967b858858f9f700aeb58b00472d1e2e233470
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-04-17 13:42:53 +0600

    first commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.css'
+const Login = () => {
+      return (
+            <div className='form-container'>
+                  <h1 className='form-title'>Login</h1>
+            </div>
+      );
+};
+
+export default Login;
